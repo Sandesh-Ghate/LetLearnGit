de265d3d8319ad9cef19d6b915d83f9bfa0a8ab0
Sandesh <ghate564@gmail.com>
2022-09-28 16:28:49 +0530

    removed b from Sum.java

--- Sum.java
+++ Sum.java
@@ -2,9 +2,8 @@ public class Sum{
 
     public static void main(String[] args){
         int a = 5;
-        int b = 6;
         int c = 8;
 
-        System.out.println("The Sum is " + (a+b+c));
+        System.out.println("The Sum is " + (a+c));
     }
 }
