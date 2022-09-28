4cab41448bd19dabdc8900f22600fb2c59d4d8bd
Sandesh <ghate564@gmail.com>
2022-09-28 16:26:33 +0530

    added c in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,7 +3,8 @@ public class Sum{
     public static void main(String[] args){
         int a = 5;
         int b = 6;
+        int c = 8;
 
-        System.out.println("The Sum is " + (a+b));
+        System.out.println("The Sum is " + (a+b+c));
     }
 }
